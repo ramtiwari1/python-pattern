0ce3a7b7a885185e31c303247183622cee2e61b2
ram <tram83594@gmail.com>
2021-12-10 10:19:25 +0530

    this is my second commit

---
+++ p2.py
@@ -0,0 +1,16 @@
+i=5
+str1=""
+while(i>=1):
+    j=5
+    while(j>i):
+        str1+=" "
+        j=j-1
+    k=1
+    while(k<=i):
+        str1+="#"
+        k=k+1
+    str1+="\n"
+    i=i-1
+print(str1)
+#pattern program    
+    
