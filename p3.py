6570e7e1c929843c6c937ee5b24cec1343c7716d
ram <tram83594@gmail.com>
2021-12-10 10:21:04 +0530

    this is my thard commit

---
+++ p3.py
@@ -0,0 +1,15 @@
+i=5
+str1=""
+while(i>=1):
+    j=1
+    while(j<=i):
+        str1+=" "
+        j=j+1
+    k=5
+    while(k>=i):
+        str1+="@"
+        k=k-1
+    str1+="\n"
+    i=i-1
+print(str1)
+#pattern program
